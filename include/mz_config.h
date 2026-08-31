12a8ab8ccac4a36d4a45340921c7de5f18b0efab
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-08-31 13:26:55 +0530

    fixes for windows compilation

--- include/mz_config.h
+++ include/mz_config.h
@@ -1,6 +1,16 @@
 #ifndef MZ_CONFIG_H
 #define MZ_CONFIG_H
 
+#if defined(_WIN32)
+#define HAVE_DIRENT_H 0
+#define HAVE_SYS_DIRENT_H 0
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_PDIR 0
+#define HAVE_FSEEKO 0
+#define HAVE_SYMLINK 0
+#define HAVE_READLINK 0
+#else
 #define HAVE_DIRENT_H 1
 #define HAVE_SYS_DIRENT_H 0
 #define HAVE_INTTYPES_H 1
@@ -9,6 +19,8 @@
 #define HAVE_FSEEKO 1
 #define HAVE_SYMLINK 1
 #define HAVE_READLINK 1
+#endif
+
 #define HAVE_ZLIB 1
 #define ZLIB_COMPAT 1
 
