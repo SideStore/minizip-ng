5003ae0a9490a9952b44c8a1f51781a7c5cf249d
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-08-31 13:19:10 +0530

    fixes for windows compilation

--- Package.swift
+++ Package.swift
@@ -8,6 +8,37 @@
 
 import PackageDescription
 
+#if os(Windows)
+let osSources = [
+    "mz_os_win32.c",
+    "mz_strm_os_win32.c"
+]
+let osExcludes = [
+    "mz_os_posix.c",
+    "mz_strm_os_posix.c"
+]
+let osDefines: [CSetting] = [
+    .define("_CRT_SECURE_NO_WARNINGS"),
+    .define("_CRT_NONSTDC_NO_WARNINGS")
+]
+let linkerLibs: [LinkerSetting] = []
+#else
+let osSources = [
+    "mz_os_posix.c",
+    "mz_strm_os_posix.c"
+]
+let osExcludes = [
+    "mz_os_win32.c",
+    "mz_strm_os_win32.c"
+]
+let osDefines: [CSetting] = [
+    .define("unix", to: "1")
+]
+let linkerLibs: [LinkerSetting] = [
+    .linkedLibrary("z")
+]
+#endif
+
 let package = Package(
     name: "minizip-ng",
     platforms: [
@@ -39,14 +70,12 @@ let package = Package(
                 "mz_crypt_openssl.c",
                 "mz_crypt_winvista.c",
                 "mz_crypt_winxp.c",
-                "mz_os_win32.c",
                 "mz_strm_bzip.c",
                 "mz_strm_bzip.h",
                 "mz_strm_libcomp.c",
                 "mz_strm_libcomp.h",
                 "mz_strm_lzma.c",
                 "mz_strm_lzma.h",
-                "mz_strm_os_win32.c",
                 "mz_strm_pkcrypt.c",
                 "mz_strm_pkcrypt.h",
                 "mz_strm_ppmd.c",
@@ -62,34 +91,29 @@ let package = Package(
                 "INDEX.md",
                 "LICENSE",
                 "README.md"
-            ],
+            ] + osExcludes,
             sources: [
                 "mz_crypt.c",
                 "mz_os.c",
-                "mz_os_posix.c",
                 "mz_strm.c",
                 "mz_strm_buf.c",
                 "mz_strm_mem.c",
-                "mz_strm_os_posix.c",
                 "mz_strm_split.c",
                 "mz_strm_zlib.c",
                 "mz_zip.c",
                 "mz_zip_rw.c"
-            ],
+            ] + osSources,
             publicHeadersPath: "include",
             cSettings: [
                 .headerSearchPath("include"),
                 .headerSearchPath("."),
-                .define("unix", to: "1"),
                 .define("HAVE_ZLIB", to: "1"),
                 .define("ZLIB_COMPAT", to: "1"),
                 .define("MZ_ZIP_NO_ENCRYPTION"),
                 .define("MZ_ZIP_NO_CRYPTO"),
                 .unsafeFlags(["-w"])
-            ],
-            linkerSettings: [
-                .linkedLibrary("z")
-            ]
+            ] + osDefines,
+            linkerSettings: linkerLibs
         )
     ],
     cLanguageStandard: .gnu11
